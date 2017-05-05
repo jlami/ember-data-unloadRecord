bdefb173815d67849e96294a9f05f40c917197e3
Joren l'Ami <joren@distinct.nl>
2017-05-05 17:24:33 +0200

    buttons for simple testing

--- app/routes/application.js
+++ app/routes/application.js
@@ -4,4 +4,27 @@ export default Ember.Route.extend({
 	model() {
 		return Ember.RSVP.hash({posts: this.store.findAll('post'), authors: this.store.findAll('author')});
 	},
+	
+	actions: {
+		fill() {
+			let author = null;
+			if (this.currentModel.authors.get('length') == 0) {
+				author = this.store.createRecord('author', {name: 'test author'});
+				author = author.save();
+			} else {
+				author = Ember.RSVP.resolve(this.currentModel.authors.get('firstObject'));
+			}
+			
+			author.then((a) => {
+				console.log(a.get('id'));
+				//TODO: maybe only one?
+				this.store.createRecord('post', {name: 'test post1', author: a}).save();
+				this.store.createRecord('post', {name: 'test post2', author: a}).save();
+			});
+		},
+		dotest() {
+			if (this.currentModel.posts.get('length') > 0)
+				this.currentModel.posts.get('firstObject').destroyRecord();
+		},
+	},
 });
